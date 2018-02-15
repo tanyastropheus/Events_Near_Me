c274329d36c1013ee360465eeeafbda1c3747f74
Tanya Kryukova <ttkryukova@gmail.com>
2018-02-15 00:25:27 +0000

    added separate endpoints to test Google Map Javascript API code

--- event_app/app.py
+++ event_app/app.py
@@ -17,9 +17,34 @@ def show_all():
     search_all = requests.get('http://localhost:9200/events/_search?pretty')
     return search_all.content
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 @app.route('/map')
 def display_map():
+    '''displays Google Map with Holberton School address hard-coded in'''
+    return render_template('add_map.html')
+
+@app.route('/map/geocoding')
+def geocoding():
+    '''drop pin based on address (using geocode to convert to long & lat)'''
+    return render_template('geocoding.html')
+
+@app.route('/map/info_window')
+def info_window():
+    '''current the same as geocoding; REVISIT'''
+    return render_template('info_window.html')
+
+@app.route('/map/geo_detect')
+def geo_detect():
+    '''detects user location based on user device'''
     return render_template('geo_detect.html')
+
+@app.route('/map/geocoding_from_db')
+def geocoding_from_db():
+    '''drop pins based on address from test data (index = event_test)'''
+    return render_template('geocoding_from_db.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
