6d2f5cfb87b440dcb2e2503bf464ebe6a464a56e
Tanya Kryukova <ttkryukova@gmail.com>
2018-05-29 06:58:47 +0000

    load data from file for analyzer testing

--- playground/es-combined_query_test.py
+++ playground/es-combined_query_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''Playing with python elasticsearch low level client'''
-import requests, json, urllib.parse
-import sys
+import requests, json, urllib.parse, sys, time
 from pprint import pprint
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, DocType
@@ -19,12 +18,12 @@ def check_setup():
 
 def delete_index(index):
     '''delete index if it exists'''
-    if es.indices.exist(index=index):
+    if es.indices.exists(index=index):
         es.indices.delete(index=index)
 
 def create_index(index):
     '''create empty index with customized setting & mapping'''
-    # customize analyzer for english stemming, possessives, and synonyms
+ # customize analyzer for english stemming, possessives, and synonyms
     english_synonym = {
         "analysis": {
             "filter": {
@@ -50,11 +49,11 @@ def create_index(index):
                 "english_synonym": {
                     "tokenizer":  "standard",
                     "filter": [
-                        "synonym",
+                        "synonym",  # does not work as well
                         "english_possessive_stemmer",
                         "lowercase",
                         "english_stop",
-                        "english_stemmer"
+                        "english_stemmer"  # play with different aggressive levels
                     ]
                 }
             }
@@ -105,7 +104,7 @@ def store_docs(index, doc_type, events):
 
 def get_num_docs(index, doc_type):
     '''return the number of docs saved in an index'''
-    if es.indices.exist(index=index):
+    if es.indices.exists(index=index):
         num_docs = es.count(index=index, doc_type=doc_type)['count']
         print("number of documents: ", num_docs)
 
@@ -144,14 +143,106 @@ def save_geo(index, doc_type, doc_id, geo_location):
     es.update(index=index, doc_type=doc_type, id=doc_id, body=geo_location)
 
 
+def read_data_from_file(filename):
+    '''read event data from test file.
+
+    Returns:
+        List of event dicts.
+    '''
+    # remove comments from file - is there a faster way to do this?
+    text = ""
+    with open(filename) as f:
+        for line in f:
+            li = line.strip()
+            if not li.startswith('#'):
+                text += line
+    events = json.loads(text)
+    return events
+
+
+def view_tokens(events, field, proc_type, proc_name):
+    '''
+    Print a list of tokens based on the analyzer/tokenizer chosen.
+
+    Args:
+        events (list of dicts): events
+        field (str): field of the event to be tokenized
+        proc_type (str): "analyzer" or "tokenizer"
+        proc_name (str): name of the proc_type
+    '''
+
+    body = {
+        proc_type: proc_name,
+        "text": ""
+    }
+
+    print('{}: {}'.format(proc_type, proc_name))
+    for i in range(len(events)):
+        text = events[i]['event' + str(i + 1)][field]
+        body['text'] = text
+        print(text)
+        results = es.indices.analyze(index=index, body=body)
+        print("tokens: ")
+
+        tokens = []
+        for j in range(len(results['tokens'])):
+            tokens.append(results['tokens'][j]['token'])
+
+        pprint(tokens)
+
+
+def search(index, doc_type, q_string):
+    '''search index for matching query docs'''
+    multi_query = {
+        'query': {
+            'multi_match': {
+                'query': q_string,
+                'type': 'best_fields',
+                'analyzer': 'english_synonym',
+                'fields': ['name', 'description', 'tags'],
+                'fuzziness': 'AUTO'
+            }
+        }
+    }
+
+    single_query = {
+        'query': {
+            'match': {
+                'description': {
+                    'query': q_string,
+                    'analyzer': 'english_synonym'
+                    }
+            }
+        }
+    }
+
+    print("query:", q_string)
+    results = es.search(index=index, doc_type=doc_type, body=multi_query)
+    pprint(results)
+
 if __name__ == '__main__':
     # delete existing index and create a new one to laod new data
-    delete_index(index=index)
-    create_index(index=index)
+
+    delete_index(index)
+    create_index(index)
+
+
+    filename = 'stemmer_test_data.py'
+    events = read_data_from_file(filename)
+#    view_tokens(events, "description", "analyzer", "english_synonym")
 
     # store new data
-    store_docs(index=index, doc_type=doc_type, events)
+    store_docs(index, doc_type, events)
 
+    # allow es server time to store all data
+    time.sleep(3)
+
+    # verify the index is not empty
+    get_num_docs(index, doc_type)
+
+    # search docs with test queries
+    search(index, doc_type, "hate")
+    '''
     # save data location in geo-coordinates
     num_docs = get_num_docs(index=index, doc_type=doc_type)
     i = 0
@@ -159,179 +250,12 @@ if __name__ == '__main__':
         geo = addr_to_geo(index=index, doc_type, i)
         save_geo(index=index, doc_type=doc_type, i, geo)
         i += 1
+    '''
+
 '''
 # update mapping after index creation
 es.indices.put_mapping(index='event_test', doc_type='event_info', body=setting['mappings']['event_info'])
 '''
-
-event1 = {
-    "address" : "533 Sutter Street, San Francisco, CA",
-    "time" : "10pm",
-    "description" : "Laugh out loud every Friday and Saturday night with Secret Improv Society: San Francisco's award-winning, interactive, late-night comedy show! Each unique show features fast-paced scenes and songs inspired by audiences and made up on the spot.\n\nPerformances are held in a cozy underground theater, conveniently located near Union Square, with a full bar, no drink minimums, and free Oreos. Itâs a perfect date night, group night, and a must-see attraction for locals and visitors alike. Shows can sell-out, so rather than take your chances at the door, get discounted tickets at https://www.secretimprov.com.\n\nNEW: To celebrate their phenomenal ten year run, Secret Improv is adding 8pm shows to their current 10pm Fri./Sat. line-up beginning June 1, 2018. The 8pm show will be the same fast-paced and interactive format as the original 10pm \"late-night\" show...only earlier. Tickets at https://www.secretimprov.com.\n",
-    "venue" : "Shelton Theater",
-    "image_url" : "https://cdn.sfstation.com/assets/images/events/0231/2312744/1490403003-2312744a_orig.jpg",
-    "date" : "Fri May 18",
-    "link" : "https://www.sfstation.com//secret-improv-society-late-night-comedy-e2312744",
-    "cost" : 19,
-    "tags" : [
-            "Comedy",
-            "Dating",
-            "Drinks",
-            "Theater / Performance Arts"
-    ],
-    "name" : "Secret Improv Society Late-Night Comedy",
-    'location': {
-        'lat': 0.0,
-        'lon': 0.0
-    }
-}
-
-event2 = {
-    "address" : "1192 Folsom Street, San Francisco, CA",
-    "time" : "10pm-2am",
-    "description" : "UHAUL SF!\nWe’re San Francisco’s Party For Girls Who Love Girls! ?\nCreating safe space for women since 2014.\n? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?\n\nGet the “URGE TO MERGE” on the dance floor with…\n\n?opening set by?\n\n? KOSLOV (San Francisco)\nKoslov is a East Bay Native going on 8 years on vinyl. This babe has opened for artists such as Syd The Kyd, DJ Theory and headlined events in NYC for pride afterparties. Koslov’s vibe is a mixture of dancehall, hiphop, dembow, soul, reggaeton and funk. Catch this part time DJ and full time 2nd grade teacher on decks from 10pm – 12am ????\n@skoslovich\nhttps://www.soundcloud.com/skoslov\n? ? ?\n\n? CHINA G (San Francisco)\nChina G is definitely a G. An OG. Mixin’ up tracks since the good ol’ Rebel Girl, Cockblock, Hotpants days. Know about those? Then you’re a muthaf*kken O.G. as well. You can skip a gym day coz’ CHINA G is gonna make you WERRRK that dance floor ????\n*Throws a backwards peace sign*\n? ? ?\n@djchinag\nhttps://www.soundcloud.com/djchinag\n\n? ? Bossy SF Entertainment GOGO GALS ? ?\n\n? ? show your ID with proof of U-hauling with your wifey & get in FREE before 10:30pm ? ? (Matching address on both ID’s)\n\n3rd Friday @ F8\n1192 Folsom Street SF\n21+\n\n$5 w/ RSVP entry before 10:30pm will be donated to The San Francisco Dyke March ?\n$15 after.\n\n? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?\n",
-    "venue" : "F8 | 1192 Folsom",
-    "image_url" : "https://cdn.sfstation.com/assets/images/events/0235/2350376/1526445905-2350376a_orig.jpg",
-    "date" : "Fri May 18",
-    "link" : "https://www.sfstation.com//uhaul-sf-ft-koslov-china-g-e2350376",
-    "tags" : [
-        "Drinks",
-        "Clubs",
-        "Music"
-    ],
-    "cost": -1,
-    "name" : "UHAUL SF ft. Koslov + China G",
-    'location': {
-        'lat': 0.0,
-        'lon': 0.0
-    }
-}
-
-event3 = {
-    "address" : "200 Larkin St, San Francisco, CA",
-    "time" : "9pm",
-    "description" : "The Center for Asian American Media presents CAAMFest36, a 15-day celebration of film, music and food. This year, we moved our fest to run right in the heart of Asian Pacific Heritage month, May 10 through the 24th. Featuring a diverse showcase of over 100 narratives, documentaries, concerts and special events, CAAMFest comes to life in over 20 venues across San Francisco and Oakland. \n\n\n\nHighlights from this year's program include:\n\n\nGala Presentations:\n\nAn American Hero: Norman Mineta and his Legacy (Opening Night)\nBitter Melon\nOrigin Story\nAunt Lily’s Flower Book (Closing Night)\n\n\nSpecial Presentations\n\nMeditation Park \nCome Drink with Me \nGolden Swallow\n\n\nMusic and Live Performances\n\nHeritage SF\nDirections in Sound - https://caamfest.com/2018/events/directions-in-sound/\nAnatomy of a Music Video with Warren Fu - https://caamfest.com/2018/events/anatomy-of-a-music-video-w-warren-fu/\nAnatomy of a Music Video with Ruby Ibarra\n\n\nFood\n\nFirst Kitchen\nJimami Tofu\nLumpia (15th Anniversary)\nUlam: Main Dish\n\n\nNarrative:\n\n20 Weeks\nDead Pigs\nEat a Bowl of Tea (Out of the Vaults)\nGo for Broke: An Origin Story\nHalf Window\nI Can, I Will, I Did\nSaving Sally\nStand Up Man\nUnlovable\nWhite Rabbit\n\n\nDocumentaries:\n\nA Little Wisdom\nA Time to Swim\nHavana Divas\nLate Life: The Chien-Ming Wang Story\nLooking For?\nMinding the Gap\nNailed It\nThe People’s Republic of Desire\nThe Registry\n\n\nShorts Programs:\n\nAltered States\nEpisodics\nFighters & Dreamers\nIn Transition\nLife, Animated\nMade you a Mixtape\nOut/Here\nWomen on the Rise\n\n\nCheck out the full program, watch trailers, and get tickets at https://caamfest.com/2018/schedule/ .\n\n\n--------\n\nCAAMFest is the nation's largest showcase for new Asian American and Asian films in San Francisco, Berkeley and Oakland. Since 1982, the festival has been an important launching point for Asian American independent filmmakers as well as a vital source for new Asian cinema.\n\nhttps://www.facebook.com/CAAMFest/\nhttps://twitter.com/caam\n",
-    "venue" : "Starline Social Club",
-    "image_url" : "https://cdn.sfstation.com/assets/images/events/0185/1853652/1454632881-1853652a_orig.jpg",
-    "date" : "Fri May 18",
-    "link" : "https://www.sfstation.com//caamfest-2018-e1853652",
-    "tags" : [
-        "Film / Television",
-        "Music",
-        "Festival / Fair"
-    ],
-    "name" : "CAAMFest 2018",
-    "cost": -1,
-    'location': {
-        'lat': 0.0,
-        'lon': 0.0
-    }
-}
-
-event4 = {
-    "address" : "474 Valencia Street, San Francisco, CA",
-    "time" : "7pm doors, 8pm performances, 10pm comedy",
-    "description" : "#ETC\n\nA weekly showcase (with food) consisting of artists, poets, musicians, and comedians. Donation based, all-ages, plus an authentic Bay Area cultural experience!\n\nHost: Larry Dorsey Jr. (comedian, writer, radio personality)\n",
-    "venue" : "Black and Brown Social Club",
-    "image_url" : "https://cdn.sfstation.com/assets/images/events/0234/2347535/1524177433-2347535a_orig.jpg",
-    "date" : "Fri May 18",
-    "link" : "https://www.sfstation.com//equiptos-timeless-cypher-e2347535",
-    "cost" : 0,
-    "tags" : [
-        "Comedy",
-        "Art",
-        "Community",
-        "Music",
-        "Theater / Performance Arts"
-    ],
-    "name" : "Equipto's Timeless Cypher",
-    'location': {
-        'lat': 0.0,
-        'lon': 0.0
-    }
-}
-
-event5 = {
-    "address" : "2781 24th Street, San Francisco, CA",
-    "time" : "8pm",
-    "description" : "Topsy-Turvy Queer Circus presents\n\nPARADISE: Belly of the Beast\n\nTopsy-Turvy Queer Circus presents the final installment of its Afrosurrealist trilogy PARADISE: Belly of the Beast. As doubt and disorder overtake her world, the fallen Angel must face her fears to unify the gods of PARADISE and defy the beast that stalks them. Told through a blend of aerial and acrobatic dance alongside vogue, hip hop and film, this epic finale features a stellar cast of queer and trans performing artists of color.\n\nPARADISE is co-produced by India Sky Davis and Indi McCasey, directed by Davis and Gabriel Christian and features Davis and former Cirque du Soleil performer Marshall Jarreau, alongside artists including Kiebpoli Calnek, Brandon Kazen-Maddox, Davia Spain, The Lady Ms. Vagina Jenkins, and Saturn Rising.'\n\nCelebrating its 6th year as part of the National Queer Arts Festival, Topsy-Turvy Queer Circus organizes multidisciplinary circus arts productions that challenge and subvert traditional ideals of beauty, gender, sexuality and power. For more info please visit topsyturvycircus.org.\n\nThis event is sponsored by Brava Theater and Queer Cultural Center with generous support from the San Francisco Arts Commission, California Arts Council, Horizons Foundation, Grants for the Arts, the Zellerbach Family Foundation, and the National Arts and Disability Center at the University of California Los Angeles.\n",
-    "venue" : "Brava Theater Center",
-    "image_url" : "https://cdn.sfstation.com/assets/images/events/0235/2350804/1526687883-2350804a_orig.jpg",
-    "date" : "Fri May 18",
-    "link" : "https://www.sfstation.com//paradise-belly-of-the-beast-e2350804",
-    "cost": -1,
-    "tags" : [
-        "Theater / Performance Arts"
-    ],
-    "name" : "PARADISE: Belly of the Beast",
-    'location': {
-        'lat': 0.0,
-        'lon': 0.0
-    }
-}
-
-event6 = {
-    "name" : "The Color Purple - A Broadway Musical",
-    "tags" : [
-        "Dance (performance)",
-        "Music",
-        "Theater / Performance Arts"
-    ],
-    "time" : "8PM",
-    "address" : "1192 Market St., San Francisco, CA",
-    "description" : "Prepare for one of the most thought-provoking, moving, emotional and ultimately uplifting musicals of the season – John Doyle’s startling, stripped-back adaption of The Color Purple. Enthusiastically received by audiences and critics, this Broadway revival has thrice the weight of the original – and that’s saying something. Empowering and culturally significant, The Color Purple gives new meaning to musical theatre as a performance medium.\n\nThe Color Purple Tickets\nOrpheum Theatre Tickets\nThings To Do In San Francisco\n",
-    "image_url" : "https://cdn.sfstation.com/assets/images/events/0234/2349132/1525469327-2349132a_orig.jpg",
-    "venue" : "SHN Orpheum Theatre",
-    'location': {
-        'lat': 0.0,
-        'lon': 0.0
-    },
-    "link" : "https://www.sfstation.com//the-color-purple-a-broadway-musical-e2349132",
-    "date" : "Fri May 18",
-    "cost": -1
-}
-
-event7 = {
-    "link" : "https://www.sfstation.com//cheaper-than-therapy-e2062971",
-    "date" : "Fri May 18",
-    "name" : "Cheaper Than Therapy",
-    "tags" : [
-        "Comedy"
-    ],
-    "time" : "10pm",
-    "address" : "406 Clement Street, San Francisco, CA",
-    "description" : "Come spend an intimate evening enjoying unadulterated stand-up comedy with some of the best comedians from the Bay Area and beyond at Cheaper Than Therapy. The show is every Thursday, Friday, and Saturday at 10pm and every Sunday at 7pm. You'll get a healthy dose of laughs at San Francisco's historic Shelton Theater. Come learn that laughter really is the best medicine.\n\nProduced by the formidable comedic trio of Eloisa Bravo, Scott Simpson, and Jon Allen, the show offers you an evening of great stand-up comedy, as well as access to a wide variety of reasonably priced alcoholic beverages. \n\nCheck in with the box office downstairs when you arrive. The bar opens at 9pm and the show starts at 10pm, so come early for cheap drinks and great seats. While everyone is welcome at the theater, the show is really only appropriate for mature audiences, so 18 and up is strongly advised due to the adult nature of the material. Hey, it is stand-up comedy after all.\n\nAfter the show, everyone over 21 is encouraged to come to an after party at the neighborhood bar.\n\nWebsite: http://cttcomedy.com\nTickets: https://cttcomedy.eventbrite.com\nYelp: https://bit.ly/cttyelp\nFacebook: https://fb.me/cttcomedy\nInstagram: https://instagram.com/cttcomedy\n\nEloisa Bravo has performed at the Punch Line, the Purple Onion, Cobb's and Rooster T. Feathers, as well as Palm Beach, Miami, and Hollywood improv shows. She has won several comedy competitions in the San Francisco Bay Area and performed at many festivals and showcases. \n\nJon Allen has performed at the Punch Line in San Francisco, Cap City Comedy Club in Austin, Texas, and regularly performs in venues throughout the New York City area. Allen's work has appeared in Wired Magazine, Tech Crunch, Savage Henry, and The Rachel Maddow Blog, in addition to his numerous physics publications. You can find him on Twitter at https://twitter.com/mathturbator.\n\nScott Simpson has performed at SF Sketchfest, the Punch Line in San Francisco, the Bridgetown Comedy Festival, and the North American Comedy and Music Festival. Simpson's show \"You Look Nice Today\" has received the Podcast Award for Best Comedy Podcast and his jokes can be found on the illustrious twitter at https://twitter.com/scottsimpson.\n\nEach week, Jon, Eloisa, and Scott are joined by the three of the best comics from the Bay Area and beyond to bring you a fresh, exciting ~75 minutes of stand-up comedy. The bar opens at 9pm on Thursday through Saturday and 6pm on Sundays, so come out early and have a few drinks before the show starts.\n",
-    "image_url" : "https://cdn.sfstation.com/assets/images/events/0206/2062971/1490895613-2062971a_orig.png",
-    "venue" : "Shelton Theater",
-    "cost": -1,
-    'location': {
-        'lat': 0.0,
-        'lon': 0.0
-    }
-}
-
-event8 = {
-    "link" : "https://www.sfstation.com//ben-nicky-e2347410",
-    "cost" : 20,
-    "date" : "Fri May 18",
-    "name" : "Ben Nicky",
-    "tags" : [
-        "Clubs",
-        "Music"
-    ],
-    "time" : "10:00 PM to 3:00 AM",
-    "address" : "540 Howard Street, San Francisco, CA",
-    "description" : "Temple Nightclub Presents Ben Nicky\n\nBen Nicky is a British trance DJ who has played at some of the world's biggest electronic music festivals, including Nature One, Tomorrowland and A State of Trance. He has racked up millions of plays across a variety of online platforms with tracks like \"Drop,\" \"The One\" and \"Hectic.\"\n\nContact us at 415.312.3668 or [email protected] for reservations.\n\nThis is a 21+ event.\n",
-    "image_url" : "https://cdn.sfstation.com/assets/images/events/0234/2347410/1526341037-2347410a_orig.jpg",
-    "venue" : "Temple SF",
-    'location': {
-        'lat': 0.0,
-        'lon': 0.0
-    }
-}
-
-
-
 '''
 # query data with specific tags
 # terms search through inverted index, which converts tokens into lowercase
