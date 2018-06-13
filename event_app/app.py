ada18447d69de08dfd7601740d8e88925677125f
Tanya Kryukova <ttkryukova@gmail.com>
2018-06-13 19:11:42 +0000

    revising endpoint logic for event tags

--- event_app/app.py
+++ event_app/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-import requests, json
-import copy
+import requests, json, copy, sys
+print(sys.path)
 from event_app.db import DB
 from pprint import pprint
 from elasticsearch import Elasticsearch
@@ -12,14 +12,16 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 db = DB('event_test', 'event_info')
+#db = DB('test_compound_search', 'test_compound_doc')
 
 # check ES is up and running
 res = requests.get('http://localhost:9200')
 print(res.status_code)
-'''
+
 # connect to ES server
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-'''
+
+
 @app.route('/index')
 def main():
     return render_template('index.html')
@@ -27,57 +29,53 @@ def main():
 @app.route('/api/event_search', methods=['POST', 'GET'])
 def event_search():
     """return events searched by the user"""
-    if request.get_json() is None:
+    params = request.get_json()
+
+    if params is None:
+        print("params is none")
         abort(404)
 
-    params = request.get_json()
-    geo_only_query = {
-        'geo_distance': {
-            'distance': params['radius'],
-            'location': {
-                'lat': params['user_location']['lat'],
-                'lon': params['user_location']['lng']
-            }
-        }
-    }
+    # check cost input
+    try:
+        float(params['cost'])
+        if float(params['cost']) < 0:
+            print("cost < 0")
+            abort(404)
+    except ValueError:
+        if params['cost'] == "100+":
+            params['cost'] = 10000
+        else:
+            print("cost abort")
+            abort(404)
 
-    cost_only_query = {
-        'range': {
-            'cost': {
-                'gte': -1, 'lte': params['cost']
-            }
-        }
-    }
+    # check radius input
+    try:
+        float(params['radius'][:-2])
+        if float(params['radius'][:-2]) <= 0:
+            print("radius negative")
+            abort(404)
+    except ValueError:
+        print("radius not a number")
+        abort(404)
 
-    # queries the specified cost & geo range,
-    # plus events that don't have listed cost
-    cost_geo_query =  {
+    cost_geo_query = {
         'bool': {
-            'must': [
-                geo_only_query, cost_only_query
-            ]
+            'must': [{
+                'geo_distance': {
+                    'distance': params['radius'],
+                    'location': {
+                        'lat': params['user_location']['lat'],
+                        'lon': params['user_location']['lng']
+                    }
+                }
+            },
+                {'range': {
+                    'cost': {
+                        'gte': -1, 'lte': params['cost']
+                    }
+                }
+             }]
         }
-    }
-
-    multi_match_query = {
-        'multi_match': {  # REVISIT: add boost to specific fields
-            'query': '',  # query strings to be matched from docs
-            'type': 'best_fields',  # the default; Finds documents which match any field, but uses the _score from the best field
-            'fields': '',
-            'fuzziness': 'AUTO'  # in case of user typo
-            # REVISIT: Set Analyzer for intelligently finding synonyms
-        }
-    }
-
-    event_query = {   # need to implement date & time range
-        'query': {
-            'bool': {
-                'must': multi_match_query,
-                'should': multi_match_query,
-                'filter': cost_geo_query
-            }
-        }
-#        'min_score': 0.25
     }
 
     all_events_query = {
@@ -88,54 +86,44 @@ def event_search():
         }
     }
 
-    if not params['keywords']:  # event tags only
-        if 'Any' in params['tags']:
-            # query all events that meet other search criteria
-            body = all_events_query
- #           data = es.search(index=index_name, doc_type=doc_type_name,
- #                            body=all_events_query)
-        else:
-            # query events with matching tags
-            # must search matching strings from event tags (AND)
-            # DESIGN DECISION: instead of doing a binary keyword event tag search,
-            # will construct an event tag string and do full-text search
-            # because the event tags from the source websites are dynamically generated
-            # and the listed event tags from the app may not reflect the current tags
-            # full-text solves the issue of synonym event tags
-            # and tags like "Art" vs "Arts"
-            must_query = copy.deepcopy(multi_match_query)
-            tag_string = ""
-            for tag in params['tags']:
-                tag_string += tag + ' '
+    event_query = {
+        'query': {
+            'bool': {
+                'must': {
+                    'multi_match': {'query': "",
+                                    'analyzer': 'english_synonym',
+                                    'fields': ['name', 'description', 'tags'],
+                                    'fuzziness': 'AUTO'
+                                }
+                },
+                'filter': cost_geo_query
+            }
+        }
+    }
 
-            # strip space at the end of tag_string
-            must_query['multi_match']['query'] = tag_string.strip()
-            must_query['multi_match']['fields'] = 'tags'
-
-            # should also search match strings from event name (OR)
-            should_query = copy.deepcopy(multi_match_query)
-            should_query['multi_match']['query'] = tag_string.strip()
-            should_query['multi_match']['fields'] = 'name'
-
-            event_query['query']['bool']['must'] = must_query
-            event_query['query']['bool']['should'] = should_query
-
-            body = event_query
-#            data = es.search(index=index_name, doc_type=doc_type_name,
-#                             body=event_query)
-    else: # keywords only
-        # full text search
-        keywords_query = copy.deepcopy(multi_match_query)
-        keywords_query['multi_match']['query'] = params['keywords']
-        keywords_query['multi_match']['fields'] = ['name', 'tags']
-        event_query['query']['bool']['must'] = keywords_query
-        del event_query['query']['bool']['should']
+    if 'Any' in params['tags']:
+        body = all_events_query
+    elif params['tags']:
+        tag_string = ""
+        for tag in params['tags']:
+            tag_string += tag + ' '
+        event_query['query']['bool']['must']['multi_match']['query'] = tag_string.strip()
         body = event_query
-#        data = es.search(index=index_name, doc_type=doc_type_name,
-#                         body=event_query)
+    elif params['keywords']:
+        event_query['query']['bool']['must']['multi_match']['query'] = params['keywords']
+        body = event_query
+    else:
+        data = {}
+
 
     data = db.search(body)
-#    pprint(data)
+    '''
+    try:
+        data = db.search(body)
+    except UnboundLocalError:
+        print("exception")
+        pass
+    '''
     return json.dumps(data)
 
 @app.route('/api/all_events')
@@ -155,6 +143,7 @@ def all_events():
     pprint(events)
     print("num_events: ", num_events)
     return json.dumps(events)
+
 
 @app.route('/map/all')
 def all_events_map():
